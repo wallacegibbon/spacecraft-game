f68ac13de443e762fa63c18a17c6d0303a1f5558
wallace <wallacegibbon@outlook.com>
2022-05-01 17:17:25 +0800

    Trivial change: The .clang-format in previous commit is incomplete, the LLVM style will put template declarations in one line by default.

--- src/common.h
+++ src/common.h
@@ -10,7 +10,10 @@ const int BASE_INTERVAL = interval_from_speed(BASE_SPEED);
 
 const int STATIC_Y_STEP = 6;
 
-template <typename T> inline T max(T a, T b) { return a > b ? a : b; }
+template <typename T>
+inline T max(T a, T b) {
+    return a > b ? a : b;
+}
 
 inline double hypotenuse(double a, double b) { return std::sqrt(a * a + b * b); }
 
